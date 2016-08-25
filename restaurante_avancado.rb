5dee9dc440a9e12776b4a610b096df61c23db569
Claudemir Mendes <claudemir.mendes@pipeline.com.br>
2016-08-25 13:28:46 -0300

    4.13 - Exercícios - Atributos e propriedades

--- restaurante_avancado.rb
+++ restaurante_avancado.rb
@@ -1,3 +1,17 @@
-def qualifica(nota, msg="Obrigado")
-  puts "A nota do restaurante foi #{nota}. #{msg}"
+class Restaurante
+
+
+	def initialize(nome)
+		puts "criando um novo restaurante #{nome}"
+		@nome = nome
+	end
+	def qualifica(nota, msg="Obrigado")
+		puts "A nota do #{@nome} foi #{nota}. #{msg}"
+	end
+
+	restaurante_um = Restaurante.new('la raclete')
+	restaurante_dois = Restaurante.new('xerife')
+
+	restaurante_um.qualifica(10)
+	restaurante_dois.qualifica(1, "Ruim!")
 end

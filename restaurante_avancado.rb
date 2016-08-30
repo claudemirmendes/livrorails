214cf88ae787ef8c7333fce8117c5b8a88503e40
Claudemir Mendes <claudemir.mendes@pipeline.com.br>
2016-08-30 14:36:45 -0300

    4.17 - Exercícios - Arrays e Hashes

--- restaurante_avancado.rb
+++ restaurante_avancado.rb
@@ -1,5 +1,5 @@
 class Restaurante
-
+	attr_accessor :nome
 
 	def initialize(nome)
 		puts "criando um novo restaurante #{nome}"
@@ -8,10 +8,22 @@ class Restaurante
 	def qualifica(nota, msg="Obrigado")
 		puts "A nota do #{@nome} foi #{nota}. #{msg}"
 	end
+	def fechar_conta(dados)
+		puts "Conta fechado no valor de #{dados[:valor]} 
+		e com nota #{dados[:nota]}. Comentário: #{dados[:comentario]}"
+	end
+
 
 	restaurante_um = Restaurante.new('companhia do churrasco')
+	restaurante_um.fechar_conta valor: 50, nota: 9, comentario: 'Gostei!'
 	restaurante_dois = Restaurante.new('xerife')
+	restaurante_tres = Restaurante.new('pão na massa')
+
 
 	restaurante_um.qualifica(10)
 	restaurante_dois.qualifica(1, "Ruim!")
+	franquia = Franquia.new
+	franquia.adiciona restaurante_um, restaurante_tres
+	#franquia.adiciona restaurante_dois
+	franquia.mostra
 end

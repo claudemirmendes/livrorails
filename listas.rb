61489e9f4de53eb6dd33c7382a76ace76ab3a0a4
Claudemir Mendes <claudemir.mendes@pipeline.com.br>
2016-09-05 12:36:33 -0300

    alterações

--- listas.rb
+++ listas.rb
@@ -1,13 +1,17 @@
 class Franquia
-	def initialize
-		@restaurantes = []		
-	end
-	def adiciona(*restaurante)
-		@restaurantes << restaurante		
-	end
-	def mostra
-		for restaurante in restaurantes
-			restaurante.nome
-		end
-	end
+
+  def initialize
+    @restaurantes = []
+  end
+
+  def adiciona(*restaurante)
+    @restaurantes << restaurante
+  end
+
+  def mostra
+    for restaurante in @restaurantes
+      puts restaurante.nome
+    end
+  end
+
 end

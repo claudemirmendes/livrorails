6e70e6dfaa3a27ce36aad95bbe3a768e14130ccb
claudemir <claudemir.mendes@pipeline.com.br>
2016-09-22 12:40:11 -0300

    pipeline

--- metaprogramacao.rb
+++ metaprogramacao.rb
@@ -4,7 +4,7 @@ class Restaurante
 		@@total || = 0
 		@@total = @@total + 1
 		puts "Restaurantes criados: #{@@total}"
-		@nome = nome
+		@nome = nome 
 	end
 	def self.relatorio
 		puts "Foram criados #{@@total} restaurantes"
